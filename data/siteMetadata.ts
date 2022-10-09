eaeec255e324f23b4fa8723e32e70c97f42b732e
asanchezyali <asanchezyali@gmail.com>
2022-10-09 16:44:21 -0500

    add site metadata

---
+++ data/siteMetadata.ts
@@ -0,0 +1,22 @@
+export const siteMetadata: { [key: string]: any } = {
+  en: {
+    headerTitle: "Alejandro Sánchez Yalí - Blog",
+    headerNavLinks: [
+      { href: "/blog", name: "Artículos" },
+      { href: "/tags", name: "Etiquetas" },
+      { href: "/projects", name: "Proyectos" },
+      { href: "/about", name: "Bio" },
+    ],
+  },
+  es: {
+    headerTitle: "Alejandro Sánchez Yalí - Blog",
+    headerNavLinks: [
+      { href: "/blog", name: "Blog" },
+      { href: "/tags", name: "Tags" },
+      { href: "/projects", name: "Projects" },
+      { href: "/about", name: "About" },
+    ],
+  },
+  theme: "system",
+  avatar: "/avatar.jpeg",
+};
