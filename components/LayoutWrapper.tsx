ebbf364012a41609a88f36723616a1993a6ea09c
asanchezyali <asanchezyali@gmail.com>
2022-10-09 16:43:01 -0500

    add main layout

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import SectionContainer from "@/components/SectionContainer";
+import Link from "@/components/Link";
+import ThemeSwitch from "@/components/ThemeSwitch";
+import LanguageSwitch from "@/components/LanguageSwitch";
+import MobileNav from "./MobileNav";
+import { siteMetadata } from "@/data/siteMetadata";
+import { LanguageContext } from "@/providers/LanguageProvider";
+
+interface LayoutWrapperProps {
+  children: React.ReactNode;
+}
+
+const LayoutWrapper = ({ children }: LayoutWrapperProps) => {
+  const { language } = React.useContext(LanguageContext);
+  return (
+    <div className="bg-slate-100 dark:bg-slate-800">
+      <SectionContainer className="fixed w-screen bg-slate-200 dark:bg-slate-900">
+        <header className="flex items-center justify-between py-3">
+          <Link
+            href="/"
+            className="text-xl font-medium text-slate-800 dark:text-slate-300"
+          >
+            {siteMetadata[language].headerTitle}
+          </Link>
+          <div className="hidden md:block">
+            <nav className="flex items-center">
+              {siteMetadata[language].headerNavLinks.map(
+                (link: { [key: string]: string }) => (
+                  <Link
+                    key={link.name}
+                    href={link.href}
+                    className="text-base font-light text-slate-800 dark:text-slate-300 px-4"
+                  >
+                    {link.name}
+                  </Link>
+                )
+              )}
+              <span className="text-2xl font-extralight text-slate-800 dark:text-slate-300 px-4">
+                |
+              </span>
+              <LanguageSwitch />
+              <ThemeSwitch />
+            </nav>
+          </div>
+          <MobileNav />
+        </header>
+      </SectionContainer>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default LayoutWrapper;
