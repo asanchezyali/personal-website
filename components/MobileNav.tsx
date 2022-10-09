d36b990c74ed9d51e1980d3d9db5aea11be092fa
asanchezyali <asanchezyali@gmail.com>
2022-10-09 16:43:43 -0500

    add mobile navbar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import Link from './Link'
+import { siteMetadata } from "@/data/siteMetadata";
+import { LanguageContext } from '@/providers/LanguageProvider';
+import LanguageSwitch from './LanguageSwitch';
+import ThemeSwitch from './ThemeSwitch';
+
+const MobileNav = () => {
+  const [navShow, setNavShow] = useState(false)
+  const { language } = React.useContext(LanguageContext);
+
+  const onToggleNav = () => {
+    setNavShow((status) => {
+      if (status) {
+        document.body.style.overflow = 'auto'
+      } else {
+        document.body.style.overflow = 'hidden'
+      }
+      return !status
+    })
+  }
+
+  return (
+    <div className="md:hidden">
+      <button
+        type="button"
+        className="h-8 w-8 rounded"
+        aria-label="Toggle Menu"
+        onClick={onToggleNav}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="text-slate-800 dark:text-slate-300"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={`fixed top-0 left-0 z-10 h-full w-full transform bg-slate-100 duration-300 ease-in-out dark:bg-slate-900 ${
+          navShow ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className="flex items-center justify-end mt-5">
+          <LanguageSwitch />
+          <ThemeSwitch />
+          <button
+            type="button"
+            className="mr-4 h-8 w-8 rounded"
+            aria-label="Toggle Menu"
+            onClick={onToggleNav}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="text-slate-800 dark:text-slate-100"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <nav className="fixed mt-2 h-full">
+          {siteMetadata[language].headerNavLinks.map((link:{ [key: string]: string }) => (
+            <div key={link.name} className="px-4 py-4">
+              <Link
+                key={link.name}
+                href={link.href}
+                className="text-base font-light tracking-widest text-slate-800 dark:text-slate-100"
+                onClick={onToggleNav}
+              >
+                {link.name}
+              </Link>
+            </div>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default MobileNav
