9980bd5c5af12f4a543276a694ef3c349fe7813e
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2022-11-03 23:13:00 -0500

    add page title component

--- components/PageTitle.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({ children }: React.PropsWithChildren<{}>) {
-  return (
-    <h1 className="text-3xl md:text-4xl font-bold text-slate-800 dark:text-slate-300">
-      {children}
-    </h1>
-  );
-}
