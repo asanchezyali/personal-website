6346d7a48baddd30234219e2d298ea2c53a607fa
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2022-11-03 23:12:12 -0500

    add MDX component

--- components/MDXComponents.tsx
+++
@@ -1,6 +0,0 @@
-import { useMemo } from "react";
-import { getMDXComponent } from "mdx-bundler/client";
-import NextImage from "next/image";
-import CustomLink from "@/components/Link";
-import TOCInline from "@/components/TOCInline";
-
