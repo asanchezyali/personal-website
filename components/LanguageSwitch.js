141655af7357650be9c63cbff30c5ba48ac7f539
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2022-11-09 00:14:48 -0500

    add language switch component

---
+++ components/LanguageSwitch.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { LanguageContext } from '@/providers/LanguageProvider'
+
+const LanguageSwitch = () => {
+  const { language, setLanguage } = React.useContext(LanguageContext)
+
+  return (
+    <button
+      aria-label="Toggle Language"
+      type="button"
+      className="rounded-md bg-slate-300 px-4 py-1 font-medium text-gray-900 dark:bg-gray-800 dark:text-gray-100"
+      onClick={() => setLanguage(language === 'en' ? 'es' : 'en')}
+    >
+      {language === 'en' ? 'ES' : 'EN'}
+    </button>
+  )
+}
+
+export default LanguageSwitch
