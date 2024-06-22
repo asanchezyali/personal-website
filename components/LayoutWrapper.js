03f27c48e02b1bda47891238ec28e7d5796d2a25
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-06-21 23:07:38 -0500

    remove logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,7 +14,7 @@ const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
+        <header className="flex items-center justify-between pt-10">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="hidden sm:block">
