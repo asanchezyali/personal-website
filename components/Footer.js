d048589f1b07c5d7507acebc55b8b5befb4759f6
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-06-22 14:37:28 -0500

    add lateral table of contents (#3)
    * update article overview
    
    * remove logo
    
    * remove tags link
    
    * update site metadata
    
    * update author layout
    
    * remove titles
    
    * update about
    
    * remove profile image
    
    * fix header title
    
    * update package.json
    
    * update article overview
    
    * add page title
    
    * load comments
    
    * update posts
    
    * add icon-link and icon classes
    
    * add useHeadObserver hook
    
    * add rehype autolink headings
    
    * update about design
    
    * update about
    
    * add lateral table of contents

--- components/Footer.js
+++ components/Footer.js
@@ -14,12 +14,9 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 flex flex-col items-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
         </div>
       </div>
     </footer>
