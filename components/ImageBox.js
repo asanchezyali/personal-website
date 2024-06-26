e169f2e2fad7b1689316cae3e04c2b6fcfeca862
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-06-25 22:04:58 -0500

    add directed graph definition

--- components/ImageBox.js
+++ components/ImageBox.js
@@ -2,10 +2,10 @@ import Image from './Image'
 
 const ImageBox = ({ src, alt, width, height, children }) => {
   return (
-    <div className="flex w-full flex-col items-center">
+    <div className="mb-[-25px] flex w-full flex-col items-center">
       <Image src={src} alt={alt} width={width} height={height} className="rounded-lg" />
       <div
-        className="mt-[-10px] text-[12px] text-gray-800 dark:text-gray-200"
+        className="mt-[-10px] text-[14px] text-gray-800 dark:text-gray-200"
         style={{ width: width }}
       >
         {children}
