51fd6966ff19d5e5f1cf02629167e82111929c63
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-09-10 12:07:49 -0500

    update SEO

--- pages/_document.js
+++ pages/_document.js
@@ -19,9 +19,30 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta
+            name="google-site-verification"
+            content="9yyV-4tBX0dUfCywA6SU7kXvc8DTQMiUem_Rzshn3Pk"
+          />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+          <meta name="robots" content="index,follow" />
+          <meta name="author" content="Alejandro Sánchez Yalí, asanchezyali@gmail.com" />
+          <meta name="url" content="https://www.asanchezyali.com" />
+          <meta name="identifier-URL" content="https://asanchezyali.com" />
+          <meta name="distribution" content="Local" />
+          <meta name="rating" content="General" />
+          <meta name="revisit-after" content="7 days" />
+          <meta name="target" content="all" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="MobileOptimized" content="320" />
+          <meta itemProp="name" content="Club Tuksury" />
+          <meta httpEquiv="Expires" content="0" />
+          <meta httpEquiv="Pragma" content="no-cache" />
+          <meta httpEquiv="Cache-Control" content="no-cache" />
+          <meta httpEquiv="imagetoolbar" content="no" />
+          <meta httpEquiv="x-dns-prefetch-control" content="off" />
+
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
