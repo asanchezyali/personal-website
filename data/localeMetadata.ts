d98431383606d4341ac7930b50d2ca5bf9d0a934
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-01-29 15:58:36 -0500

    🎁 Add localeMetadata.ts for multilingual title and description management

---
+++ data/localeMetadata.ts
@@ -0,0 +1,13 @@
+type Metadata = {
+  [locale: string]: string
+}
+
+export const maintitle: Metadata = {
+  en: 'Smart, Precise, and Limitless Solutions',
+  es: 'Soluciones inteligentes, precisas y sin límites',
+}
+
+export const maindescription: Metadata = {
+  en: 'Smart, Precise, and Limitless Solutions',
+  es: 'Soluciones inteligentes, precisas y sin límites',
+}
