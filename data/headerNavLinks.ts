80c9ab8a0581fc58d34c7723ec135abfd973871d
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-01-29 15:58:29 -0500

    ✨ Add headerNavLinks.ts for navigation link management

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,7 @@
+const headerNavLinks: { href?: string; title: string }[] = [
+  { href: '/', title: 'Home' },
+  // { href: '/projects', title: 'Projects' },
+  // { href: '/blog', title: 'Blog' },
+]
+
+export default headerNavLinks
