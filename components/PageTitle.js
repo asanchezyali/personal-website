20533c43391b4a1993d4ae3a796af1f9d5b707b9
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-01-28 22:22:11 -0500

    🗑️ Remove unused components: Link, MDXComponents, MobileNav, NewsletterForm, PageTitle

--- components/PageTitle.js
+++
@@ -1,7 +0,0 @@
-export default function PageTitle({ children }) {
-  return (
-    <h1 className="mt-8 text-2xl font-extrabold  text-gray-900  md:text-3xl dark:text-gray-100">
-      {children}
-    </h1>
-  )
-}
