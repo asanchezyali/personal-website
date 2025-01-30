989f732b08b3d417493e60e42d0b048136597ea3
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-01-30 18:04:30 -0500

    🔮 Unhide Blog link in header navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks: { href?: string; title: string }[] = [
   { href: '/', title: 'Home' },
   // { href: '/projects', title: 'Projects' },
-  // { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'Blog' },
 ]
 
 export default headerNavLinks
