d4021ead02e92699b0596158b73c413ac426aa14
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-01-30 19:29:27 -0500

    🔧 Update ESLint configuration, add NVM version, and enhance tag data for AI topics

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,23 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
-    amd: true,
     node: true,
     es6: true,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jsx-a11y', 'prettier'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
-    'next',
     'next/core-web-vitals',
   ],
   parserOptions: {
-    project: true,
+    project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
   rules: {
     'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -32,11 +28,10 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
-    'react/prop-types': 0,
-    '@typescript-eslint/no-unused-vars': 0,
-    'react/no-unescaped-entities': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/no-unescaped-entities': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
   },
-}
+};
