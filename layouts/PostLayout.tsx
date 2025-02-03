af8afb0583873b84faa9eb47ef828075e215362a
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-03 17:54:22 -0500

    🔧 Refactor PostLayout component by removing author details section and replacing footer with aside for improved structure

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -6,7 +6,6 @@ import WalineComments from '@/components/comments/walinecomponents/walineComment
 import Link from '@/components/mdxcomponents/Link'
 import PageTitle from '@/components/PageTitle'
 import SectionContainer from '@/components/SectionContainer'
-import Image from '@/components/mdxcomponents/Image'
 import Tag from '@/components/tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/scroll'
@@ -73,44 +72,7 @@ export default async function PostLayout({
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">{t('authors')}</dt>
-              <dd>
-                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Link href={`/${locale}/about/${author.slug}`}>
-                          <Image
-                            src={author.avatar}
-                            width={38}
-                            height={38}
-                            alt="avatar"
-                            title="avatar"
-                            className="h-10 w-10 rounded-full"
-                          />
-                        </Link>
-                      )}
-                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                        <dt className="sr-only">{t('name')}</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
+
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               {series && (
                 <div className="not-prose mt-4">
@@ -136,7 +98,7 @@ export default async function PostLayout({
                 )}
               </div>
             </div>
-            <footer>
+            <aside>
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
                 {tags && (
                   <div className="py-4 xl:py-8">
@@ -184,7 +146,7 @@ export default async function PostLayout({
                   &larr;{t('back')}
                 </Link>
               </div>
-            </footer>
+            </aside>
           </div>
         </div>
       </article>
