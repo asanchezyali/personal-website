bcff3c10f23e1bd13a0cef2e2853bdbaa0685876
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-06 18:24:42 -0500

    🥎 Update English and Spanish localization for blog section, adding insights and descriptions for enhanced user engagement

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -28,23 +28,6 @@ export default function Footer() {
     <>
       <footer>
         <div className="mt-16 flex flex-col items-center">
-          <div className="mb-3 flex space-x-4">
-            <div className="flex items-center">
-              {siteMetadata.formspree === false ? (
-                <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-              ) : (
-                <button className="flex items-center focus:outline-none" onClick={ContactClick}>
-                  <SocialIcon kind="mail" size={6} />
-                </button>
-              )}
-            </div>
-            <div className="flex items-center">
-              <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-            </div>
-            <div className="flex items-center">
-              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-            </div>
-          </div>
           <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
             <div>{siteMetadata.author}</div>
             <div>{` • `}</div>
