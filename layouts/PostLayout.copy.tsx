89ff2eb478b5619955ad9696dc4f45deba021c1d
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-14 18:01:16 -0500

    🛸 Update author field to use standardized format across multiple blog posts

--- layouts/PostLayout.copy.tsx
+++ layouts/PostLayout.copy.tsx
@@ -15,11 +15,11 @@ import { PostSeriesBox } from '@/components/seriescard'
 import Share from '@/components/share'
 import { Toc } from 'pliny/mdx-plugins'
 import Sidetoc from '@/components/sidetoc'
-import Image from '@/components/mdxcomponents/Image'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
+
 const postDateTemplate: Intl.DateTimeFormatOptions = {
   weekday: 'long',
   year: 'numeric',
@@ -44,86 +44,68 @@ export default async function PostLayout({
   children,
   params: { locale },
 }: LayoutProps) {
-  const { filePath, path, slug, date, title, tags, language, series, toc, headerImage } = content
+  const { filePath, path, slug, date, title, tags, language, series, toc } = content
   const basePath = path.split('/')[0]
   const { t } = await createTranslation(locale, 'home')
   const tableOfContents: Toc = toc as unknown as Toc
-
-  console.log(tags)
-
   return (
     <>
       <ScrollTopAndComment />
-      <article className="py-8">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
-          <div className="lg:col-span-3">
-            <header className="mb-8 text-center">
-              {headerImage && (
-                <div className="relative mb-10 h-[200px] w-full sm:h-[500px]">
-                  <Image
-                    src={headerImage}
-                    fill
-                    style={{ objectFit: 'cover', objectPosition: 'center' }}
-                    className="rounded-lg"
-                    alt={title}
-                    priority={true}
-                  />
+      <Sidetoc toc={tableOfContents} />
+      <article>
+        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+          <header className="pt-6 xl:pb-6">
+            <div className="space-y-1 text-center">
+              <dl className="space-y-10">
+                <div>
+                  <dt className="sr-only">{t('pub')}</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={date}>
+                      {new Date(date).toLocaleDateString(language, postDateTemplate)}
+                    </time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+            </div>
+          </header>
+          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
+
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              {series && (
+                <div className="not-prose mt-4">
+                  <PostSeriesBox data={series} />
                 </div>
               )}
-              <dl className="mb-2">
-                <dt className="sr-only">{t('pub')}</dt>
-                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                  <time dateTime={date}>
-                    {new Date(date).toLocaleDateString(language, postDateTemplate)}
-                  </time>
-                </dd>
-              </dl>
-              <PageTitle>{title}</PageTitle>
-            </header>
-            <div className="prose max-w-none dark:prose-invert">{children}</div>
-            <footer className="mt-8">
-              <div className="mt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
-                {siteMetadata.iswaline && <WalineComments />}
-                {siteMetadata.comments && siteMetadata.iscomments && <Comments slug={slug} />}
+              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
+                <Link href={discussUrl(path)} rel="nofollow">
+                  {t('twitter')}
+                </Link>
+                {` • `}
+                <Link href={editUrl(filePath)}>{t('github')}</Link>
               </div>
-            </footer>
-          </div>
-          <aside className="lg:col-span-1">
-            <div className="sticky top-8 hidden text-base font-medium lg:block">
+              <Share title={title} slug={slug} />
+              <div
+                className="mt-10 pb-6 pt-6 text-center text-gray-700 dark:text-gray-300"
+                id="comment"
+              >
+                {siteMetadata.iswaline === true && <WalineComments />}
+                {siteMetadata.comments && siteMetadata.iscomments === true && (
+                  <Comments slug={slug} />
+                )}
+              </div>
+            </div>
+            <aside>
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
-                {authorDetails.map((author) => (
-                  <li className="mb-8 flex items-center space-x-2" key={author.name}>
-                    {author.avatar && (
-                      <Image
-                        src={author.avatar}
-                        width="38"
-                        height="38"
-                        alt="avatar"
-                        className="h-10 w-10 rounded-full"
-                      />
-                    )}
-                    <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                      <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                      <dd>
-                        {author.twitter && (
-                          <Link
-                            href={author.linkedin as string}
-                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          >
-                            {author.twitter.replace('https://twitter.com/', '@')}
-                          </Link>
-                        )}
-                      </dd>
-                    </dl>
-                  </li>
-                ))}
-
                 {tags && (
                   <div className="py-4 xl:py-8">
-                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                      {t('tags')}:
-                    </h2>
-                    <div className="flex flex-col">
+                    <p className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </p>
+                    <div className="flex flex-wrap">
                       {tags.map((tag) => (
                         <Tag key={tag} text={tag} />
                       ))}
@@ -155,11 +137,18 @@ export default async function PostLayout({
                   </div>
                 )}
               </div>
-            </div>
-          </aside>
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href={`/${locale}/${basePath}`}
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                  aria-label="Back to the blog"
+                >
+                  &larr;{t('back')}
+                </Link>
+              </div>
+            </aside>
+          </div>
         </div>
-
-        <Sidetoc toc={tableOfContents} />
       </article>
     </>
   )
