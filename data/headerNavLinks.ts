75c64f11dc7ec7af2ab554ab0c3ebf99f6e2b4eb
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-17 21:53:03 -0500

    🛠️ Add "Services" link to header navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks: { href?: string; title: string }[] = [
   { href: '/', title: 'Home' },
   // { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
+  { href: '/services', title: 'Services' },
 ]
 
 export default headerNavLinks
