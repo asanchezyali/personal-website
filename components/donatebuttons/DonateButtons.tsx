8b8941e30474cadaed34620379ed886e00e9eae4
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-19 21:10:57 -0500

    🌟 Update DonateButtons component to change tip link and button text

--- components/donatebuttons/DonateButtons.tsx
+++ components/donatebuttons/DonateButtons.tsx
@@ -15,13 +15,13 @@ const DonateButtons = () => {
       </a>
 
       <a
-        href="https://brave.com/tip"
+        href="https://www.paypal.com/paypalme/asanchezyali"
         target="_blank"
         rel="noopener noreferrer"
         className="flex h-8 items-center rounded-full bg-white px-4 text-sm font-medium text-gray-700 shadow hover:bg-red-50 hover:text-red-600 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:hover:text-red-400"
       >
         <Heart className="mr-2 h-4 w-4 text-red-500" />
-        Tip BAT
+        Tip me
       </a>
     </div>
   )
