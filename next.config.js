4a46d8f834cc7f98b1c8cdd9d1ec1651d0564cfc
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2025-02-24 13:33:21 -0500

    🎨 Update Content Security Policy to include Google Analytics and improve code formatting (#18)
    * 📝 Update README links and refactor ScrollTopAndComment component for improved functionality
    
    * 🌟 Add Brave publisher verification file, update DonateButtons component, and create Spanish author profile
    
    * 🌟 Update DonateButtons component to change tip link and button text
    
    * 🌀 Adjust positioning of navigation and sidebar components for improved layout
    
    * 🎾 Add Google site verification meta tag and enable Google Analytics integration
    
    * 🌐 Update site URL and repository link in site metadata
    
    * 🎨 Refactor HeroSection component for improved styling and layout
    
    * 🎨 Remove unnecessary background classes for improved styling consistency
    
    * 🎳 Enhance MobileNav and TocBody components for improved responsiveness and styling
    
    * 🎨 Update Content Security Policy to include Google Analytics and improve code formatting

--- next.config.js
+++ next.config.js
@@ -1,48 +1,42 @@
-const { withContentlayer } = require('next-contentlayer2')
+const { withContentlayer } = require('next-contentlayer2');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-// You might need to insert additional domains in script-src if you are using external services
+// Actualización de la Content Security Policy para permitir Google Analytics
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is statichunt.com http://www.youtube.com https://www.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data: statichunt.com;
   media-src 'self' *.s3.amazonaws.com;
-  connect-src * statichunt.com;
+  connect-src * statichunt.com https://www.google-analytics.com https://www.googletagmanager.com;
   font-src 'self';
-  frame-src giscus.app https://www.youtube.com/ https://www.youtube-nocookie.com/
+  frame-src giscus.app https://www.youtube.com/ https://www.youtube-nocookie.com/;
 `;
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
@@ -53,7 +47,7 @@ const securityHeaders = [
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer]
+  const plugins = [withContentlayer, withBundleAnalyzer];
   return plugins.reduce((acc, next) => next(acc), {
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
@@ -66,7 +60,6 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'api.qrserver.com',
           pathname: '**',
-
         },
         {
           protocol: 'https',
@@ -81,15 +74,15 @@ module.exports = () => {
           source: '/(.*)',
           headers: securityHeaders,
         },
-      ]
+      ];
     },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      })
+      });
 
-      return config
+      return config;
     },
-  })
-}
+  });
+};
