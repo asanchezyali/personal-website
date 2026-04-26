7097c3752b0725d0db35e8b27559296b1ab929f1
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2026-04-26 00:53:29 -0500

    chore: remove _reference directory from tracking, add to .gitignore

--- _reference/data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks: { href?: string; title: string }[] = [
-  { href: '/', title: 'Home' },
-  // { href: '/projects', title: 'Projects' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/collaborate', title: 'Collaborate' },
-]
-
-export default headerNavLinks
